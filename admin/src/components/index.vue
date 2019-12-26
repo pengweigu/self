01656035d5e48b7e28735361cba1d9a58290e0f7
pengweigu <18518666740@163.com>
2019-12-26 17:43:08 +0800

    admin

--- admin/src/components/index.vue
+++ admin/src/components/index.vue
@@ -1,46 +1,45 @@
 <template>
   <div class="app">
     <el-container>
-      <el-aside class="app-side app-side-left"
-                :class="isCollapse ? 'app-side-collapsed' : 'app-side-expanded'">
+      <el-aside class="app-side app-side-left" :class="isCollapse ? 'app-side-collapsed' : 'app-side-expanded'">
         <div class="app-side-logo">
-          <img src="@/assets/logo.png"
-               :width="isCollapse ? '60' : '60'"
-               height="60" />
+          <img src="@/assets/head.png" :width="isCollapse ? '60' : '60'" height="60" />
         </div>
         <div>
           <!-- 我是样例菜单 -->
-          <el-menu default-active="1-4-1"
-                   class="el-menu-vertical-demo"
-                   @open="handleOpen"
-                   :collapse="isCollapse">
+          <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" :collapse="isCollapse">
             <el-submenu index="1">
               <template slot="title">
                 <i class="el-icon-location"></i>
                 <span slot="title">导航一</span>
               </template>
+
               <el-menu-item-group>
                 <span slot="title">分组一</span>
                 <el-menu-item index="1-1">选项1</el-menu-item>
                 <el-menu-item index="1-2">选项2</el-menu-item>
               </el-menu-item-group>
+
               <el-menu-item-group title="分组2">
                 <el-menu-item index="1-3">选项3</el-menu-item>
               </el-menu-item-group>
+
               <el-submenu index="1-4">
                 <span slot="title">选项4</span>
                 <el-menu-item index="1-4-1">选项1</el-menu-item>
               </el-submenu>
             </el-submenu>
+
             <el-menu-item index="2">
               <i class="el-icon-menu"></i>
               <span slot="title">导航二</span>
             </el-menu-item>
-            <el-menu-item index="3"
-                          disabled>
+
+            <el-menu-item index="3" disabled>
               <i class="el-icon-document"></i>
               <span slot="title">导航三</span>
             </el-menu-item>
+
             <el-menu-item index="4">
               <i class="el-icon-setting"></i>
               <span slot="title">导航四</span>
@@ -51,17 +50,12 @@
 
       <el-container>
         <el-header class="app-header">
-          <div style="width: 60px; cursor: pointer;"
-               @click.prevent="toggleSideBar">
+          <div style="width: 60px; cursor: pointer;" @click.prevent="toggleSideBar">
             <i v-show="!isCollapse" class="el-icon-d-arrow-left"></i>
             <i v-show="isCollapse" class="el-icon-d-arrow-right"></i>
           </div>
           <!-- 我是样例菜单 -->
-          <el-menu default-active="1"
-                   class="el-menu-demo tab-page"
-                   mode="horizontal"
-                   @select="handleSelect"
-                   active-text-color="#409EFF">
+          <el-menu default-active="1" class="el-menu-demo tab-page" mode="horizontal" @select="handleSelect" active-text-color="#409EFF">
             <el-menu-item index="1">处理中心</el-menu-item>
             <el-submenu index="2">
               <template slot="title">我的工作台</template>
@@ -75,16 +69,14 @@
                 <el-menu-item index="2-4-3">选项3</el-menu-item>
               </el-submenu>
             </el-submenu>
-            <el-menu-item index="3"
-                          disabled>消息中心</el-menu-item>
+            <el-menu-item index="3" disabled>消息中心</el-menu-item>
             <el-menu-item index="4">
               <a href="#">订单管理</a>
             </el-menu-item>
           </el-menu>
 
           <div class="app-header-userinfo">
-            <el-dropdown trigger="hover"
-                         :hide-on-click="false">
+            <el-dropdown trigger="hover" :hide-on-click="false">
               <span class="el-dropdown-link">
                 {{ username }}
                 <i class="el-icon-arrow-down el-icon--right"></i>
@@ -92,8 +84,7 @@
               <el-dropdown-menu slot="dropdown">
                 <el-dropdown-item>我的消息</el-dropdown-item>
                 <el-dropdown-item>设置</el-dropdown-item>
-                <el-dropdown-item divided
-                                  @click.native="logout">退出登录</el-dropdown-item>
+                <el-dropdown-item divided @click.native="logout">退出登录</el-dropdown-item>
               </el-dropdown-menu>
             </el-dropdown>
           </div>
@@ -150,4 +141,7 @@
 </script>
 
 <style>
+  body {
+    background-image: url("/static/imgs/logo_index.png");
+  }
 </style>
