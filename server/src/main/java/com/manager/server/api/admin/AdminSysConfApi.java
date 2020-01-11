8ed629b92a580ee7365a91b6f17767579e89c74b
pengweigu <18518666740@163.com>
2020-01-11 19:42:47 +0800

    sys_conf

---
+++ server/src/main/java/com/manager/server/api/admin/AdminSysConfApi.java
@@ -0,0 +1,10 @@
+package com.manager.server.api.admin;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/sys")
+public class AdminSysConfApi {
+
+}
