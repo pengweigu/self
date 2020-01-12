9c8559b6e4d52257e855364bd5164c59be618def
pengweigu <18518666740@163.com>
2020-01-12 10:31:27 +0800

    user edit view

--- admin/src/components/user/userList.vue
+++ admin/src/components/user/userList.vue
@@ -1,49 +1,68 @@
 <template>
-  <el-table
-    :data="tableData"
-    stripe
-    style="width: 100%">
-    <el-table-column
-      prop="account"
-      label="账号">
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="名称">
-    </el-table-column>
-    <el-table-column
-      prop="status"
-      label="状态">
-    </el-table-column>
-    <el-table-column
-      prop="phone"
-      label="手机">
-    </el-table-column>
-    <el-table-column
-      prop="sex"
-      label="性别">
-    </el-table-column>
-    <el-table-column
-      prop="email"
-      label="邮箱">
-    </el-table-column>
-    <el-table-column
-      label="操作">
-      <template slot-scope="scope">
-        <el-button type="text" size="small" @click="viewAdminUser(scope.row)">查看</el-button>
-        <el-button type="text" size="small" @click="editAdminUser(scope.row)">编辑</el-button>
-        <el-button type="text" size="small" @click="deleteAdminUser(scope.row)">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div><!--一个模板只能有一个艮节点  多个时使用div包起来-->
+    <el-table
+      :data="tableData"
+      stripe
+      style="width: 100%">
+      <el-table-column
+        prop="account"
+        label="账号">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="名称">
+      </el-table-column>
+      <el-table-column
+        prop="status"
+        label="状态">
+      </el-table-column>
+      <el-table-column
+        prop="phone"
+        label="手机">
+      </el-table-column>
+      <el-table-column
+        prop="sex"
+        label="性别">
+      </el-table-column>
+      <el-table-column
+        prop="email"
+        label="邮箱">
+      </el-table-column>
+      <el-table-column
+        label="操作">
+        <template slot-scope="scope">
+          <el-button type="text" size="small" @click="viewAdminUser(scope.row)">查看</el-button>
+          <el-button type="text" size="small" @click="editAdminUser(scope.row)">编辑</el-button>
+          <el-button type="text" size="small" @click="deleteAdminUser(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!--编辑窗口-->
+    <el-dialog title="编辑" :visible.sync="editDialog" width="50%" :before-close="handleClose">
+      <span>编辑信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editDialog = false">取 消</el-button>
+        <el-button type="primary" @click="">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!--查看窗口-->
+    <el-dialog title="查看" :visible.sync="viewDialog" width="50%" :before-close="handleClose">
+      <span>查看信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="viewDialog = false">取 消</el-button>
+        <el-button type="primary" @click="">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
-
 <script>
   export default {
     name:'userList',
     data() {
       return {
-        tableData: []
+        tableData: [],
+        editDialog: false,
+        viewDialog: false,
       }
     },
     created() {
@@ -77,10 +96,12 @@
         });
       },
       editAdminUser(row){
-        vm.$message.error("编辑数据["+row.name+"]异常！");
+        const vm = this;
+        vm.editDialog = true;
       },
       viewAdminUser(row){
-        vm.$message.error("查看数据["+row.name+"]异常！");
+        const vm = this;
+        vm.viewDialog = true;
       }
     }
   }
